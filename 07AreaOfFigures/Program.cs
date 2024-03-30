2e48d7afb7718e81e3ea79d81e610446500b9a25
dkostadinov <d78mdd@gmail.com>
2024-03-30 12:28:51 +0200

    Add project files.

---
+++ 07AreaOfFigures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figura = Console.ReadLine();
+
+            if (figura == "square")
+            {
+                double num = double.Parse(Console.ReadLine());
+                double lice = num * num;
+                string formatted = lice.ToString("F3");
+                Console.WriteLine(formatted);
+            }
+            else if (figura == "rectangle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                double lice = num1 * num2;
+                string formatted = lice.ToString("F3");
+                Console.WriteLine(formatted);
+            }
+            else if (figura == "circle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double lice = Math.PI * num1 * num1;
+                string formatted = lice.ToString("F3");
+                Console.WriteLine(formatted);
+            }
+            else if (figura == "triangle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                double lice = num1 * num2 / 2;
+                string formatted = lice.ToString("F3");
+                Console.WriteLine(formatted);
+            }
+        }
+    }
+}
